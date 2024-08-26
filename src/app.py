371106d54884c32fba9fe99cede23682d8a3f888
Harry Day <Harry.Day.HD@Gmail.com>
2024-08-26 22:58:21 +0100

    explore the different tables in equity dataset

--- src/app.py
+++ src/app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import nasdaqdatalink as nasdaq
 
-def fetch_data(ticker: str):
+def fetch_data(table: str, ticker: str):
     try:
-        data = nasdaq.get_table('SHARADAR/SEP', ticker={ticker})
+        data = nasdaq.get_table(table, ticker={ticker})
         return data
     except Exception as e:
         st.error(f"Error fetching data: {e}")
@@ -15,12 +15,36 @@ def main():
 
     ticker = st.text_input("Enter ticker symbol (e.g., AAPL):", value="AAPL")
     
-    if st.button("Fetch Data"):
-        data = fetch_data(ticker)
+    if st.button("Fetch SHARADAR/SEP"):
+        data = fetch_data("SHARADAR/SEP", ticker)
         if data is not None:
             st.write(f"Showing data for {ticker}")
             st.dataframe(data.head(10)) 
             st.line_chart(data['close'])
+
+    if st.button("Fetch SHARADAR/ACTIONS"):
+        data = fetch_data("SHARADAR/ACTIONS", ticker)
+        if data is not None:
+            st.write(f"Showing data for {ticker}")
+            st.dataframe(data.head(10)) 
+
+    if st.button("Fetch SHARADAR/TICKERS"):
+        data = fetch_data("SHARADAR/TICKERS", ticker)
+        if data is not None:
+            st.write(f"Showing data for {ticker}")
+            st.dataframe(data.head(10)) 
+
+    if st.button("Fetch SHARADAR/METRICS"):
+        data = fetch_data("SHARADAR/METRICS", ticker)
+        if data is not None:
+            st.write(f"Showing data for {ticker}")
+            st.dataframe(data.head(10)) 
+
+    if st.button("Fetch SHARADAR/INDICATORS (not ticker specific)"):
+        data = fetch_data("SHARADAR/INDICATORS", None)
+        if data is not None:
+            st.write(f"Showing data for all tables")
+            st.dataframe(data.head(100)) 
 
 if __name__ == "__main__":
     main()
