33b6417db1470759d852387035a5ae5b397b1eb5
Harry Day <Harry.Day.HD@Gmail.com>
2024-09-01 15:46:01 +0100

    add a notebook for data fetch and related tooling

--- src/pages/2_Datasource_Caching.py
+++ src/pages/2_Datasource_Caching.py
@@ -1,20 +1,43 @@
 import streamlit as st
 import nasdaqdatalink as nasdaq
+import pandas as pd
 
+sample_tickers = pd.DataFrame([
+    ("AAPL","APPLE INC"),
+    ("AXP","AMER EXPRESS CO"),
+    ("BA","BOEING CO"),
+    ("CAT","CATERPILLAR INC"),
+    ("CSCO","CISCO SYSTEMS"),
+    ("CVX","CHEVRON CORP"),
+    ("DD","DU PONT (EI) DE"),
+    ("DIS","DISNEY WALT"),
+    ("GE","GENL ELECTRIC"),
+    ("GS","GOLDMAN SACHS"),
+    ("HD","HOME DEPOT"),
+    ("IBM","INTL BUS MACH"),
+    ("INTC","INTEL CORP"),
+    ("JNJ","JOHNSON & JOHNS"),
+    ("JPM","JPMORGAN CHASE"),
+    ("KO","COCA COLA CO"),
+    ("MCD","MCDONALDS CORP"),
+    ("MMM","3M CO"),
+    ("MRK","MERCK & CO INC"),
+    ("MSFT","MICROSOFT CORP"),
+    ("NKE","NIKE INC-B"),
+    ("PFE","PFIZER INC"),
+    ("PG","PROCTER & GAMBL"),
+    ("TRV","TRAVELERS COS"),
+    ("UNH","UNITEDHEALTH GP"),
+    ("UTX","UTD TECHS CORP"),
+    ("V","VISA INC-A"),
+    ("VZ","VERIZON COMM"),
+    ("WMT","WALMART INC"),
+    ("XOM","EXXON MOBIL CRP"),
+], columns=['Ticker', 'Company Name'])
 
-def fetch_data(table: str, ticker: str):
-    try:
-        data = nasdaq.get_table(table, ticker={ticker})
-        return data
-    except Exception as e:
-        st.error(f"Error fetching data: {e}")
-        return None
+st.write("Sample tickers")
+st.dataframe(sample_tickers)
 
-ticker = st.text_input("Enter ticker symbol (e.g., AAPL):", value="AAPL")
-
-if st.button("Fetch SHARADAR/SEP"):
-    data = fetch_data("SHARADAR/SEP", ticker)
-    if data is not None:
-        st.write(f"Showing data for {ticker}")
-        st.dataframe(data.head(10)) 
-        st.line_chart(data['close'])
+if st.button("Download all data"):
+    price_data = nasdaq.get_table("SHARADAR/SEP")
+    st.dataframe (price_data)
